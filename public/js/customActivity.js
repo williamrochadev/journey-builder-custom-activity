c7f15997d93de6b2f76731ee67a555ad84108e3a
Lennon Vinicius Alves Dias <lennon.t1340.randstad@xpi.com.br>
2019-11-05 10:15:35 -0300

    refactor initial test

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -7,17 +7,20 @@ define([
 
     var connection = new Postmonger.Session();
     var authTokens = {};
+    var eventDefinitionKey = '';
+    var templateName = '';
     var payload = {};
-    var templateName = "";
+
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
     connection.on('requestedTokens', onGetTokens);
     connection.on('requestedEndpoints', onGetEndpoints);
-
+    connection.on('requestedInteraction', requestedInteractionHandler);
     connection.on('clickedNext', save);
 
     function onRender() {
+        // JB will respond the first time 'ready' is called with 'initActivity'
         connection.trigger('ready');
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
@@ -27,73 +30,76 @@ define([
             templateName = $('#templateCode').val();
             document.getElementById('toggleActive').disabled = true;
             document.getElementById('toggleActive').innerHTML = "Ativado";
-
-            console.log('[onRender] templateName', templateName);
         });
+    }
+
+    function requestedInteractionHandler(settings) {
+        try {
+            eventDefinitionKey = settings.triggers[0].metaData.eventDefinitionKey;
+            $('#select-entryevent-defkey').val(eventDefinitionKey);
+
+            if (settings.triggers[0].type === 'SalesforceObjectTriggerV2' &&
+                settings.triggers[0].configurationArguments &&
+                settings.triggers[0].configurationArguments.eventDataConfig) {
+
+                // This workaround is necessary as Salesforce occasionally returns the eventDataConfig-object as string
+                if (typeof settings.triggers[0].configurationArguments.eventDataConfig === 'stirng' ||
+                    !settings.triggers[0].configurationArguments.eventDataConfig.objects) {
+                    settings.triggers[0].configurationArguments.eventDataConfig = JSON.parse(settings.triggers[0].configurationArguments.eventDataConfig);
+                }
+
+                settings.triggers[0].configurationArguments.eventDataConfig.objects.forEach((obj) => {
+                    deFields = deFields.concat(obj.fields.map((fieldName) => {
+                        return obj.dePrefix + fieldName;
+                    }));
+                });
+
+                deFields.forEach((option) => {
+                    $('#select-id-dropdown').append($('<option>', {
+                        value: option,
+                        text: option
+                    }));
+                });
+
+                $('#select-id').hide();
+                $('#select-id-dropdown').show();
+            } else {
+                $('#select-id-dropdown').hide();
+                $('#select-id').show();
+            }
+        } catch (e) {
+            console.error(e);
+            $('#select-id-dropdown').hide();
+            $('#select-id').show();
+        }
     }
 
     function initialize(data) {
         if (data) {
             payload = data;
         }
-
-        templateName = payload['arguments'].templateName;
-
-        console.log('data', data);
-        console.log('[initialize] templateName', templateName);
-        console.log('arguments', payload['arguments'])
-
-        if (templateName) {
-            document.getElementById('templateCode').disabled = true;
-            document.getElementById('templateCode').value = templateName;
-            document.getElementById('toggleActive').disabled = true;
-            document.getElementById('toggleActive').innerHTML = "Ativado";
-        }
-
-        var hasInArguments = Boolean(
-            payload['arguments'] &&
-            payload['arguments'].execute &&
-            payload['arguments'].execute.inArguments &&
-            payload['arguments'].execute.inArguments.length > 0
-        );
-
-        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
-
-        console.log('inArguments', inArguments);
-
-        connection.trigger('updateButton', {
-            button: 'next',
-            text: 'done',
-            visible: true
-        });
     }
 
     function onGetTokens(tokens) {
-        console.log('tokens', tokens);
+        console.log(tokens);
         authTokens = tokens;
     }
 
     function onGetEndpoints(endpoints) {
-        console.log('endpoints', endpoints);
+        console.log(endpoints);
     }
 
     function save() {
-        console.log('[save] templateName', templateName);
-
-        payload.name = "Send Whatsapp HSM";
-
-        payload['arguments'] = payload['arguments'] || {};
-        payload['arguments'].templateName = templateName;
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
+            "contactIdentifier": "{{Contact.Key}}",
+            "phoneNumber": `{{Event.${eventDefinitionKey}.\"Phone\"}}`,
             "templateName": templateName
         }];
-        
+
         payload['metaData'].isConfigured = true;
 
-        console.log('payload', payload);
-        console.log('payload arguments', payload['arguments']);
-        console.log('payload stringfy', JSON.stringify(payload));
+        console.log(JSON.stringify(payload));
 
         connection.trigger('updateActivity', payload);
     }
