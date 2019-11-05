fa177ca3ee0f825e4c89ab03d9ca8f9dbc498be5
Lennon Vinicius Alves Dias <lennon.t1340.randstad@xpi.com.br>
2019-11-05 12:48:30 -0300

    get mobilephone using attribute

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -71,7 +71,7 @@ define(function (require) {
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
             "contactIdentifier": "{{Contact.Key}}",
-            "phoneNumber": "{{Contact.MobilePhone}}",
+            "phoneNumber": "{{Contact.Attribute.MobilePhone}}",
             // "phoneNumber": `{{Event.${eventDefinitionKey}.\"Phone\"}}`,
             "templateName": templateName
         }];
